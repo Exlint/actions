a167af32d27351e9e2257f6b616ffcc48e76a3d4
Tal Rofe <tal@exlint.io>
2022-07-24 19:41:44 +0300

    refactor: 💡 update inflint
    update inflint

--- inflint.config.ts
+++ inflint.config.ts
@@ -1,18 +1,9 @@
 import type { Config } from '@exlint.io/inflint';
 
 const inflintConfig: Config = {
-	aliases: {
-		'[UIComponent]': `ED([A-Z][a-z0-9]+)((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?`,
-	},
 	rules: {
-		'{scripts,assets,docker}/**/*': [2, 'kebab-case'],
+		'{scripts,src}/**/*': [2, 'kebab-case'],
 		'**/*.yml': 2,
-		'apps/frontend/**/*.css': 2,
-		'apps/backend/src/**/*': [2, 'kebab-case.point'],
-		'apps/frontend/src/{assets,data,hooks,i18n,interfaces,store,utils,styles}/**/*': [2, 'kebab-case'],
-		'apps/frontend/src/pages/**/*': [2, 'PascalCase.Point'],
-		'apps/frontend/src/components/{containers,layout}/**/*.{tsx,scss}': [2, 'PascalCase.Point'],
-		'apps/frontend/src/components/containers,ui/**/*.{tsx,scss}': [2, '[UIComponent]'],
 	},
 };
 
